87ad1dd8d926b95a761e45b2f03cafb93389ac6e
jun-dave-wamar <jundavewamar@gmail.com>
2023-04-30 01:45:46 +0800

    Version 0.0.38

--- api/controllers/Product.js
+++ api/controllers/Product.js
@@ -1,6 +1,6 @@
 const express = require("express")
 
-const Product = require("../models/product")
+const Product = require("../models/Product")
 
 //GET /products
 async function getProduct(req, res){
