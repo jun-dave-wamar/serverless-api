8de618fe26d0bcdc40cf87015ebd231d351825d0
jun-dave-wamar <jundavewamar@gmail.com>
2023-04-29 23:34:43 +0800

    Version 0.0.29

--- api/controllers/User/login/index.js
+++ api/controllers/User/login/index.js
@@ -3,15 +3,13 @@ const router = express.Router();
 const User = require("../../../models/User");
 const connection = require("../../../../db");
 require("dotenv").config();
-var bcrypt = require("bcryptjs");
-const { createToken } = require("../../../../middleware/auth");
 
-connection();
+
 
 router.post("/", async (req, res) => {
   try {
-      console.log("Successfuly");
-      
+    
+
   } catch (err) {
     console.log(err);
     res.status(500).send("Server Error");
