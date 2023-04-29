c461590c240b0b1f98f2931f2a204bc75711e047
jun-dave-wamar <jundavewamar@gmail.com>
2023-04-29 21:22:25 +0800

    Version 0.0.6

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const app = express();
 require("dotenv").config();
 
+//Import Controllers
+const userRouter = require("./api/controllers/User");
+
+const app = express();
+
 const corsOptions = {
-  origin: ["http://localhost:3001", "http://localhost:3000"],
-  // origin: ["https://v001.are-ai.ae", "https://v001.are-ai.ae/", "https://are-ai.ae", "https://are-ai.ae/"],
+  //origin: ["http://localhost:3001", "http://localhost:3000"],
+  origin: ["https://v001.are-ai.ae", "https://v001.are-ai.ae/", "https://are-ai.ae", "https://are-ai.ae/"],
   method: ["GET", "POST", "PATCH", "PUT", "HEAD", "OPTIONS"],
   credentials: true,
 };
@@ -14,12 +18,6 @@ const corsOptions = {
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json());
-
-
-//Middleware Authentication
-
-//Import Controllers
-const userRouter = require("./api/controllers/User");
 
 
 //Routes
