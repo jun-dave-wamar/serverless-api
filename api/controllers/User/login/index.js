8c6dd81864de5b1ceefe04d7e39da306f83d8fe9
jun-dave-wamar <jundavewamar@gmail.com>
2023-04-29 23:35:46 +0800

    Version 0.0.30

--- api/controllers/User/login/index.js
+++ api/controllers/User/login/index.js
@@ -1,10 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../../../models/User");
-const connection = require("../../../../db");
-require("dotenv").config();
-
-
 
 router.post("/", async (req, res) => {
   try {
